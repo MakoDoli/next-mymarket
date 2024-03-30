c482ca6392827843bca21e686abede33a4ac85ca
MakoDoli <makodolidze@hotmail.com>
2024-03-30 18:38:30 +0400

    fix urls

--- src/components/ui/ProductDetails.tsx
+++ src/components/ui/ProductDetails.tsx
@@ -5,6 +5,23 @@ import React from "react";
 import AdContent from "./AdContent";
 import Link from "next/link";
 import { translate } from "@/utils/translate";
+import { NextRequest } from "next/server";
+import { revalidatePath } from "next/cache";
+
+export async function GET(request: NextRequest) {
+  const path = request.nextUrl.searchParams.get("path");
+
+  if (path) {
+    revalidatePath(path);
+    return Response.json({ revalidated: true, now: Date.now() });
+  }
+
+  return Response.json({
+    revalidated: false,
+    now: Date.now(),
+    message: "Missing path to revalidate",
+  });
+}
 
 type Props = {
   id: number;
