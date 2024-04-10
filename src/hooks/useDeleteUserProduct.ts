0c8542dfd6d40986cb4539e79cd0cf373debfd0f
MakoDoli <makodolidze@hotmail.com>
2024-04-11 03:51:56 +0400

    cancel usedelete hook

--- src/hooks/useDeleteUserProduct.ts
+++ src/hooks/useDeleteUserProduct.ts
@@ -1,20 +1,20 @@
-import { useMutation } from "@tanstack/react-query";
-import toast from "react-hot-toast";
-import { useRouter } from "next/navigation";
-import { deleteUserProduct } from "@/services/getUsersProducts";
+// import { useMutation } from "@tanstack/react-query";
+// import toast from "react-hot-toast";
+// import { useRouter } from "next/navigation";
+// import { deleteUserProduct } from "@/services/getUsersProducts";
 
-export function useDeleteUserProduct() {
-  const router = useRouter();
+// export function useDeleteUserProduct() {
+//   const router = useRouter();
 
-  const { mutate: deleteProduct, isPending } = useMutation({
-    mutationFn: deleteUserProduct,
+//   const { mutate: deleteProduct, isPending } = useMutation({
+//     mutationFn: ()=> deleteUserProduct,
 
-    onSuccess: () => {
-      router.push("/my-products");
-      toast.success("პროდუქტი წაშლილია");
-    },
-    onError: (err) => toast.error(err.message),
-  });
+//     onSuccess: () => {
+//       router.push("/my-products");
+//       toast.success("პროდუქტი წაშლილია");
+//     },
+//     onError: (err) => toast.error(err.message),
+//   });
 
-  return { deleteProduct, isPending };
-}
+//   return { deleteProduct, isPending };
+// }
