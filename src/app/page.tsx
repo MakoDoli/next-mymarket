7e867c86d483ce642110bf3f1a1c9a1e43bf5089
MakoDoli <makodolidze@hotmail.com>
2024-04-11 03:55:57 +0400

    remove revalidation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import CategoriesCarousel from "@/components/categories/CategoriesCarousel";
-import Header from "@/components/header/Header";
 import DisplayAllProducts from "@/components/mainSection/DisplayAllProducts";
 import Spinner from "@/components/ui/Spinner";
-import { revalidatePath } from "next/cache";
 import { Suspense } from "react";
-revalidatePath("/");
+
 export default function Home() {
   return (
     <main className="xl:px-8">
