090eb41d85a81248e746f5669a6c05d4c4ffbbca
MakoDoli <makodolidze@hotmail.com>
2025-05-19 01:01:25 +0400

    supabase debugging

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -8,6 +8,9 @@ export const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 
 const supabaseKey = String(process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log("SUPABASE ANON KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
